4184fee165256543f9a4ddbf164c469edd963d7a
SrushtiKange <154757414+SrushtiKange@users.noreply.github.com>
2023-12-31 23:52:07 +0530

    Add files via upload

---
+++ Assignment 10/Ass10-1/app/comp-failure.directive.ts
@@ -0,0 +1,25 @@
+import { Directive,ElementRef,Host,HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appCompFailure]',
+  standalone: true
+})
+export class CompFailureDirective 
+{
+  constructor(private obj: ElementRef) 
+  {}
+
+ @HostListener('mouseenter') onmouseenter()
+ {
+  this.obj.nativeElement.style.color = 'red';
+ }
+
+  @HostListener('mouseleave') onmouseleave()
+  {
+    this.obj.nativeElement.style.color = 'black';
+  }
+}
+
+ 
+
+
