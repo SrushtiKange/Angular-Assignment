01fbdd8a93ec0a219b60ccb10fcd39f2420f430e
SrushtiKange <154757414+SrushtiKange@users.noreply.github.com>
2024-01-13 10:46:32 +0530

    Add files via upload

---
+++ Assignment 12/Ass-12/app/buginfo/buginfo.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+
+
+
+@Component({
+  selector: 'app-buginfo',
+  standalone: true,
+  imports: [ CommonModule],
+  templateUrl: './buginfo.component.html',
+  styleUrl: './buginfo.component.css',
+ 
+})
+export class BuginfoComponent {
+
+
+}
