ff4d64abd424f91319eda6eaab2becacba1826d7
SrushtiKange <154757414+SrushtiKange@users.noreply.github.com>
2024-01-15 01:40:38 +0530

    Add files via upload

---
+++ Assignment 14/2Ass-14/app/books/books.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-books',
+  standalone: true,
+  imports: [ CommonModule],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent 
+{
+  books = ['C++','Object oriented programming'];
+
+}
